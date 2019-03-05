331f54c697db51677c09a78f1518fa2900c736ab
yoxavi82 <xavier.num8@hotmail.com>
2019-03-05 15:51:16 +0100

    part dos

--- src/operacions/Operacions.java
+++ src/operacions/Operacions.java
@@ -17,5 +17,15 @@ public class Operacions {
         }
         return aux;
     }
+	public double getRecursiva(int n){
+        if(n < 0){
+            throw  new IllegalArgumentException();
+        }
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        Operacions fmenos1 = new Operacions();
+        return n * fmenos1.getRecursiva(n - 1);
+    }
 
 }
